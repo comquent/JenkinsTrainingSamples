6f678620550f1ec703f5e5c976c8399079127d7f
andreas.schoenfeld@comquent.de <andreas.schoenfeld@comquent.de>
2018-02-23 22:53:51 +0100

    first commit

--- vars/info.groovy
+++ vars/info.groovy
@@ -1,15 +1,22 @@
 def info(message) {
-    echo "\033[34m [INFO] ${message} \033[0m"
+	ansiColor('xterm') {
+		echo "\033[34m [INFO] ${message} \033[0m"
 }
 
 def warning(message) {
-    echo "\033[31m [WARNING] ${message} \033[0m"
+	ansiColor('xterm') {
+		echo "\033[31m [WARNING] ${message} \033[0m"
+	}
 }
 
 def error(message) {
-    echo "\033[1;31m [ERROR] ${message} \033[0m"
+	ansiColor('xterm') {
+		echo "\033[1;31m [ERROR] ${message} \033[0m"
+	}
 }
 
 def success(message) {
-    echo "\033[1;32m [SUCCESS] ${message} \033[0m"
+	ansiColor('xterm') {
+		echo "\033[1;32m [SUCCESS] ${message} \033[0m"
+	}
 }
