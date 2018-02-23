4e462fad62118875f7ded9b5e43dcafc7ab6b203
andreas.schoenfeld@comquent.de <andreas.schoenfeld@comquent.de>
2018-02-23 22:30:52 +0100

    first commit

--- vars/info.groovy
+++ vars/info.groovy
@@ -1,7 +1,15 @@
 def info(message) {
-    echo "INFO: ${message}"
+    echo "\033[34m [INFO] ${message} \033[0m"
 }
 
 def warning(message) {
-    echo "WARNING: ${message}"
+    echo "\033[31m [WARNING] ${message} \033[0m"
+}
+
+def error(message) {
+    echo "\033[1;31m [ERROR] ${message} \033[0m"
+}
+
+def success(message) {
+    echo "\033[1;32m [SUCCESS] ${message} \033[0m"
 }
